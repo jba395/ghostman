a59cc9a91c0d3e9fca5e25f0ae6f8132efa29579
jamburgey3 <jamburgey3@gatech.edu>
2022-09-13 20:29:10 -0400

    Add working function to sum particular outcome, given a dataset

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
 from src.stat_parser import Stat_Parser as sp
+from src.stat_calculator import Stat_Calculator as sc
+import src.constants as c
 
 def main():
     parser_client = sp()
@@ -7,7 +9,11 @@ def main():
         'Outcome': 'HR'
     })
 
+    calc_client = sc()
+    total = calc_client.get_total_outcome(parser_client.data, c.OUTCOME_OUTSIDE_PARK_HOMERUN)
+
     print(data.to_string())
+    print(total)
 
 if __name__ == '__main__':
     main()
--- src/stat_calculator.py
+++ src/stat_calculator.py
@@ -1,9 +1,13 @@
+import src.constants as c
+from src.stat_parser import Stat_Parser as sp
+
 class Stat_Calculator:
     def __init__(self):
-        pass
+        parser_client = sp()
 
     def get_player_stats(self, player):
         pass
 
-    def get_total(self, player, outcome):
-        pass
+    def get_total_outcome(self, data, outcome):
+        total = data[c.COLUMN_OUTCOME].str.contains(outcome).sum()
+        return total
